b97ec2fefc070d3ec3fa5d2016563391a53f70a9
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-08-26 11:20:30 +0800

    fix: explicit type annotation

--- app/LossFunction.hs
+++ app/LossFunction.hs
@@ -3,5 +3,5 @@ module LossFunction where
 lossFunction :: [Double] -> [Double] -> Double
 lossFunction predicted actual = sumSquaredErrors / fromIntegral (length predicted)
   where
-    sumSquaredErrors = sum $ zipWith (\p a -> (p - a) ^ 2) predicted actual
+    sumSquaredErrors = sum $ zipWith (\p a -> (p - a) ^ (2 :: Int)) predicted actual
 
