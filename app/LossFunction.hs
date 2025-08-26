7d703e0ac4196f96091ceaa374fe66d075ab0805
Nathan Ryver <tantaouibahaabusiness@gmail.com>
2025-08-26 11:13:19 +0800

    fix: simplified the mean squared error calculation to use zipWith and sum

--- app/LossFunction.hs
+++ app/LossFunction.hs
@@ -1,14 +1,7 @@
 module LossFunction where
--- mean squared error
+
 lossFunction :: [Double] -> [Double] -> Double
-lossFunction [] [] = 0
-lossFunction (x:xs) (y:ys) =
-  let numerator   = (x - y) ^ 2 + fst (recurse xs ys)
-      denominator = 1 + snd (recurse xs ys)
-  in numerator / fromIntegral denominator
+lossFunction predicted actual = sumSquaredErrors / fromIntegral (length predicted)
   where
-    recurse [] [] = (0, 0)  
-    recurse (a:as) (b:bs) =
-      let (sumRest, countRest) = recurse as bs
-      in ((a - b) ^ 2 + sumRest, 1 + countRest)
+    sumSquaredErrors = sum $ zipWith (\p a -> (p - a) ^ 2) predicted actual
 
